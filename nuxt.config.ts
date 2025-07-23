c3bc0864ab8b274cd51b54a591bf13a572fc3d12
joshuakitong <joshuakitong@gmail.com>
2025-07-23 20:58:47 +0800

    refactor: fix app file structure and remove ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['@/app.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   app: {
     baseURL: '/',
